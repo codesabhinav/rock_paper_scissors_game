50430bdd32a86f6492d1d8a74193ee4f37d1b9af
Abhinav Singh <113774414+codesabhinav@users.noreply.github.com>
2023-08-09 23:26:51 +0530

    Add files via upload

---
+++ rpsgame.js
@@ -0,0 +1,47 @@
+const myplayer = document.querySelector("#myplayer");
+const mycomputer = document.querySelector("#mycomputer");
+const myresult = document.querySelector("#myresult");
+const choiceBtns = document.querySelectorAll(".choiceBtn");
+let player;
+let computer;
+let result;
+
+choiceBtns.forEach(button => button.addEventListener("click", () => {
+
+    player = button.textContent;
+    computerTurn();
+    myplayer.textContent = `Player: ${player}`;
+    mycomputer.textContent = `Computer: ${computer}`;
+    myresult.textContent = checkWinner();
+}));
+
+function computerTurn(){
+
+    const randNum = Math.floor(Math.random() * 3) + 1;
+
+    switch(randNum){
+      case 1:
+        computer = "ROCK";
+        break;
+      case 2:
+        computer = "PAPER";
+        break;
+      case 3:
+        computer = "SCISSORS";
+        break;
+    }
+}
+function checkWinner(){
+    if(player == computer){
+      return "Draw!";
+    }
+    else if(computer == "ROCK"){
+      return (player == "PAPER") ? "You Win!" : "You Lose!"
+    }
+    else if(computer == "PAPER"){
+      return (player == "SCISSORS") ? "You Win!" : "You Lose!"
+    }
+    else if(computer == "SCISSORS"){
+      return (player == "ROCK") ? "You Win!" : "You Lose!"
+    }
+}
---
+++ rpsgame.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="rpsgame.css">
+</head>
+<body>
+    <div>
+        <h1 class="gameText" id="myplayer">Player: </h1>
+        <h1 class="gameText" id="mycomputer">Computer: </h1>
+        <h1 class="gameText" id="myresult">Result: </h1>
+
+        <button class="choiceBtn">ROCK</button>
+        <button class="choiceBtn">PAPER</button>
+        <button class="choiceBtn">SCISSORS</button>
+    </div>
+    <script src="rpsgame.js"></script>
+</body>
+</html>
